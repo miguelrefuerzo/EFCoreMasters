0286899c0e96059f25e6de11ba9a1f132c6e3994
2194058 <2194058@LTPH022753>
2023-07-19 16:36:50 +0800

    session 4 files

---
+++ Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.DataLayer/EfClasses/LineItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAppEFCore.DataLayer.EfClasses
+{
+    public class LineItem
+    {
+        public int LineItemId { get; set; }
+        public short NumOfProducts { get; set; }
+        public decimal ProductPrice { get; set; }
+
+        //relationships
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public Product SelectedProduct { get; set; }
+
+    }
+}
---
+++ Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.DataLayer/InventoryAppEfCoreContext.cs
@@ -0,0 +1,28 @@
+﻿using InventoryAppEFCore.DataLayer.EfClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAppEFCore.DataLayer
+{
+    public class InventoryAppEfCoreContext : DbContext
+    {
+      
+        public InventoryAppEfCoreContext(DbContextOptions<InventoryAppEfCoreContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<PriceOffer> PriceOffers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TO DO Fluent API
+        }
+
+    }
+}
