f8f2360f38d14ef7ebabf641241c2d0b63c6d206
2194058 <2194058@LTPH022753>
2023-07-26 17:05:47 +0800

    Session5 assignment

--- Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.DataLayer/EfClasses/LineItem.cs
+++ Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.DataLayer/EfClasses/LineItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,9 @@ namespace InventoryAppEFCore.DataLayer.EfClasses
         public decimal ProductPrice { get; set; }
 
         //relationships
+        [ForeignKey(nameof(Order))]
         public int OrderId { get; set; }
+        [ForeignKey(nameof(Product))]
         public int ProductId { get; set; }
         public Product SelectedProduct { get; set; }
 
