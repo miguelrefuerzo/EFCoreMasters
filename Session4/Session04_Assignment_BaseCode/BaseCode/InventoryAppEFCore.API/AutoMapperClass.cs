7ba1a628990e1173967be42604c9c90a0eb6fadc
2194058 <2194058@LTPH022753>
2023-08-01 23:54:14 +0800

    session6

--- Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.API/AutoMapperClass.cs
+++ Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.API/AutoMapperClass.cs
@@ -7,13 +7,20 @@ namespace InventoryAppEFCore.API
     public class AutoMapperClass : Profile
     {
         public AutoMapperClass() { 
-            CreateMap<Product, ProductDTO>();
-            CreateMap<LineItem, LineItemDTO>();
-            CreateMap<Order, OrderDTO>();
-            CreateMap<PriceOffer, PriceOfferDTO>();
-            CreateMap<Review, ReviewDTO>();
-            CreateMap<Supplier, SupplierDTO>();
-            CreateMap<Tag, TagDTO>();
+            CreateMap<Product, ProductDTO>()
+                .ForMember(p => p.ProductId, s => s.MapFrom(x => x.ProductId));
+            CreateMap<LineItem, LineItemDTO>()
+                .ForMember(p => p.LineItemId, s => s.MapFrom(x => x.LineItemId));
+            CreateMap<Order, OrderDTO>()
+                .ForMember(p => p.OrderId, s => s.MapFrom(x => x.OrderId));
+            CreateMap<PriceOffer, PriceOfferDTO>()
+                .ForMember(p => p.PriceOfferID, s => s.MapFrom(x => x.PriceOfferId));
+            CreateMap<Review, ReviewDTO>()
+                .ForMember(p => p.ReviewId, s => s.MapFrom(x => x.ReviewId));
+            CreateMap<Supplier, SupplierDTO>()
+                .ForMember(p => p.SupplierId, s => s.MapFrom(x => x.SupplierId));
+            CreateMap<Tag, TagDTO>()
+                .ForMember(p => p.TagId, s => s.MapFrom(x => x.TagId));
         }
     }
 }
