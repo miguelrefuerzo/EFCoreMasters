0933993fda6ae1f7221b6569382db759f3b46c60
2194058 <2194058@LTPH022753>
2023-08-17 19:28:30 +0800

    update seed and add view

---
+++ Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.DataLayer/Migrations/20230817111810_client-migration.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryAppEFCore.DataLayer.Migrations
+{
+    public partial class clientmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DeleteData(
+            //    table: "Products",
+            //    keyColumn: "ProductId",
+            //    keyValue: 1);
+
+            //migrationBuilder.DeleteData(
+            //    table: "Products",
+            //    keyColumn: "ProductId",
+            //    keyValue: 2);
+
+            //migrationBuilder.InsertData(
+            //    table: "Clients",
+            //    columns: new[] { "ClientId", "IsDeleted", "Name" },
+            //    values: new object[] { 1, false, "Client 1" });
+
+            //migrationBuilder.InsertData(
+            //    table: "Clients",
+            //    columns: new[] { "ClientId", "IsDeleted", "Name" },
+            //    values: new object[] { 2, false, "Client 2" });
+
+            //migrationBuilder.InsertData(
+            //    table: "Clients",
+            //    columns: new[] { "ClientId", "IsDeleted", "Name" },
+            //    values: new object[] { 3, false, "Client 3" });
+
+            migrationBuilder.AddViewViaSql<ClientView>("ClientView", "Client", "Name = 'Client1'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DeleteData(
+            //    table: "Clients",
+            //    keyColumn: "ClientId",
+            //    keyValue: 1);
+
+            //migrationBuilder.DeleteData(
+            //    table: "Clients",
+            //    keyColumn: "ClientId",
+            //    keyValue: 2);
+
+            //migrationBuilder.DeleteData(
+            //    table: "Clients",
+            //    keyColumn: "ClientId",
+            //    keyValue: 3);
+
+            //migrationBuilder.InsertData(
+            //    table: "Products",
+            //    columns: new[] { "ProductId", "IsDeleted", "Name" },
+            //    values: new object[] { 1, false, "Product 1" });
+
+            //migrationBuilder.InsertData(
+            //    table: "Products",
+            //    columns: new[] { "ProductId", "IsDeleted", "Name" },
+            //    values: new object[] { 2, false, "Product 2" });
+        }
+    }
+}
---
+++ Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.DataLayer/Migrations/20230817112648_clients-migration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryAppEFCore.DataLayer.Migrations
+{
+    public partial class clientsmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Clients",
+                table: "Clients");
+
+            migrationBuilder.RenameTable(
+                name: "Clients",
+                newName: "Client");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Client",
+                table: "Client",
+                column: "ClientId");
+
+            migrationBuilder.AddViewViaSql<ClientView>("ClientView", "Client", "Name = 'Client1'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Client",
+                table: "Client");
+
+            migrationBuilder.RenameTable(
+                name: "Client",
+                newName: "Clients");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Clients",
+                table: "Clients",
+                column: "ClientId");
+        }
+    }
+}
