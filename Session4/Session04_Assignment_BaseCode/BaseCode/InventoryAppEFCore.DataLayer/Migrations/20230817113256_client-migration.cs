a151ec2953e5b111d564a88d4a813bbbfe255340
2194058 <2194058@LTPH022753>
2023-08-17 19:33:46 +0800

    add seed and add view

---
+++ Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.DataLayer/Migrations/20230817113256_client-migration.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryAppEFCore.DataLayer.Migrations
+{
+    public partial class clientmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Clients",
+                table: "Clients");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.RenameTable(
+                name: "Clients",
+                newName: "Client");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Client",
+                table: "Client",
+                column: "ClientId");
+
+            migrationBuilder.InsertData(
+                table: "Client",
+                columns: new[] { "ClientId", "IsDeleted", "Name" },
+                values: new object[] { 1, false, "Client 1" });
+
+            migrationBuilder.InsertData(
+                table: "Client",
+                columns: new[] { "ClientId", "IsDeleted", "Name" },
+                values: new object[] { 2, false, "Client 2" });
+
+            migrationBuilder.InsertData(
+                table: "Client",
+                columns: new[] { "ClientId", "IsDeleted", "Name" },
+                values: new object[] { 3, false, "Client 3" });
+
+            migrationBuilder.AddViewViaSql<ClientView>("ClientView", "Client", "Name = 'Client1'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Client",
+                table: "Client");
+
+            migrationBuilder.DeleteData(
+                table: "Client",
+                keyColumn: "ClientId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Client",
+                keyColumn: "ClientId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Client",
+                keyColumn: "ClientId",
+                keyValue: 3);
+
+            migrationBuilder.RenameTable(
+                name: "Client",
+                newName: "Clients");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Clients",
+                table: "Clients",
+                column: "ClientId");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "IsDeleted", "Name" },
+                values: new object[] { 1, false, "Product 1" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "IsDeleted", "Name" },
+                values: new object[] { 2, false, "Product 2" });
+        }
+    }
+}
---
+++ Session4/Session04_Assignment_BaseCode/BaseCode/InventoryAppEFCore.DataLayer/ViewExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAppEFCore.DataLayer
+{
+    public static class ViewExtension
+    {
+        public static void AddViewViaSql<T>(
+           this MigrationBuilder migrationBuilder,
+           string viewName,
+           string tableName,
+           string whereSql)
+           where T : class
+        {
+            if (!migrationBuilder.IsSqlServer())
+                throw new NotImplementedException("This command only works for SQL Server");
+
+            var selectNamesString = string.Join(", ",
+                typeof(T).GetProperties()
+                .Select(x => x.Name));
+
+            var viewSql =
+                $"CREATE OR ALTER VIEW {viewName} AS " +
+                $"SELECT {selectNamesString} FROM {tableName} " +
+                $"WHERE {whereSql}";
+
+            migrationBuilder.Sql(viewSql);
+        }
+    }
+}
